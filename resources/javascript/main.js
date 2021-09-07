bf00984cc05e97acc5a1552c26147eff742cc1f9
christianflorea <90270143+christianflorea@users.noreply.github.com>
2021-09-07 11:43:39 -0400

    Update main.js

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["challange?", "improved technique?", "structured program?", "team environment?"];
+const textArray = ["challenge?", "improved technique?", "structured program?", "team environment?"];
 const typingDelay = 100;
 const erasingDelay = 75;
 const newTextDelay = 2000; // Delay between current and next text
